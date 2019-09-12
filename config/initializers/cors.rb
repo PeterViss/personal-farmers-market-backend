b9671df130914274b72bb8d71348aef81ff93716
Peter Viss <peter.c.viss@biola.edu>
2019-09-11 20:49:47 -0400

    temp cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
-
+#https://www.farmersmarket4u.com
 # Avoid CORS issues when API is called from the frontend app.
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://www.farmersmarket4u.com'
+    origins '*'
 
     resource '*',
       headers: :any,
